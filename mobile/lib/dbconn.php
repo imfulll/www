8edfe4ec2a6b2242af76ed17552adf88623759de
imfulll <imfulll@naver.com>
2022-12-27 07:27:26 +0900

    update

--- mobile/lib/dbconn.php
+++ mobile/lib/dbconn.php
@@ -1,6 +1,13 @@
 <?
-    $connect=mysql_connect( "localhost", "imfulll", "qwer!10101") or  
+    $connect=mysql_connect( "localhost", "full", "1234") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
-    mysql_select_db("imfulll",$connect);
+    mysql_select_db("full_db",$connect);
 ?>
+
+<?
+    // $connect=mysql_connect( "localhost", "imfulll", "qwer!10101") or  
+    //     die( "SQL server에 연결할 수 없습니다."); 
+
+    // mysql_select_db("imfulll",$connect);
+ ?>
